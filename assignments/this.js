dbccbd49cc76eea5579417d4d525960210d5676c
fromthehart <fromthehart@pm.me>
2019-05-08 12:08:07 -0400

    this.js: Principles 1 - 3 complete

--- assignments/this.js
+++ assignments/this.js
@@ -1,25 +1,56 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. In global scope, "this" refers to the window or console
+* 2. When using dot notation, "this" refers to the object before the dot
+* 3. When using a constructor, "this" refers to a specific instance of an object returned by the constructor function
+* 4. Using the call() or apply() method overrides this and what the constructor objects are set to
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
-
 // code example for Window Binding
 
-// Principle 2
+console.log("Principle 1: Window Binding");
+function iLove(school) {
+  //console.log(this);
+  return `I love ${school}!`;
+}
+console.log(iLove("Lambda School"));
 
+
+// Principle 2
 // code example for Implicit Binding
+console.log("\nPrinciple 2: Implicit Binding");
+const iSpy = {
+  preamble: 'I spy with my little eye',
+  speak: function(letter) {
+    console.log(`${this.preamble} something that begins with '${letter.toUpperCase()}'...`);
+    //console.log(this);
+  }
+}
+iSpy.speak('A');
+
 
 // Principle 3
-
 // code example for New Binding
+
+console.log("\nPrinciple 3: New Binding");
+function iSpyGame(letter) {
+  this.preamble = 'I spy with my little eye';
+  this.letter = letter;
+  this.speak = function() {
+    console.log(`${this.preamble} something that begins with '${this.letter.toUpperCase()}'...`);
+    //console.log(this);
+  }
+}
+
+const michael = new iSpyGame('M');
+const steph = new iSpyGame('S');
+
+michael.speak();
+steph.speak();
 
 // Principle 4
 
